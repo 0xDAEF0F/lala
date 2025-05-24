95529b3fb0fad2da297d77ec5c860412fc72d134
Alex T <aletapia@proton.me>
2025-05-24 13:50:38 -0600

    Integrate mic recorder plugin and update dependencies
    - Added `tauri-plugin-mic-recorder` to Cargo.toml and updated capabilities in default.json to include mic recorder permissions.
    - Integrated mic recording functionality in lib.rs, allowing for audio recording within the Tauri application.
    - Updated Cargo.lock with new dependencies to support the mic recorder features.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
+#![allow(unused)]
+
+use tauri::Manager;
 use tauri_plugin_clipboard_manager::ClipboardExt as _;
+use tauri_plugin_mic_recorder::{start_recording, stop_recording};
 use tauri_plugin_notification::NotificationExt as _;
 
 #[tauri::command]
@@ -10,6 +14,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_clipboard_manager::init())
+		.plugin(tauri_plugin_mic_recorder::init())
 		.plugin(tauri_plugin_notification::init())
 		.plugin(tauri_plugin_opener::init())
 		.setup(|app| {
@@ -21,6 +26,17 @@ pub fn run() {
 				.body("Tauri is awesome")
 				.show()
 				.unwrap();
+
+			// todo: fix this
+			let app_handle = app.handle().clone();
+			tauri::async_runtime::spawn(async move {
+				dbg!(start_recording(app_handle).await);
+				tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+				let res = stop_recording().await;
+				println!("res: {:?}", res);
+			});
+
+			_ = stop_recording();
 
 			Ok(())
 		})
