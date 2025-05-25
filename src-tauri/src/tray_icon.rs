a4e68211a09e4e0c85b591e6b3b0b83524440a0c
Alex T <aletapia@proton.me>
2025-05-24 23:48:46 -0600

    Enhance tray icon functionality and update .gitignore
    - Added event handling for left and right mouse clicks on the tray icon to start/stop recording and show the menu, respectively.
    - Updated .gitignore to exclude backup files from version control.

--- src-tauri/src/tray_icon.rs
+++ src-tauri/src/tray_icon.rs
@@ -1,6 +1,8 @@
+use crate::{start_async_task, stop_async_task, IS_RECORDING};
+use std::sync::atomic::Ordering;
 use tauri::{
 	menu::{Menu, MenuItem},
-	tray::TrayIconBuilder,
+	tray::{MouseButton, MouseButtonState, TrayIconBuilder},
 	App,
 };
 
@@ -11,7 +13,7 @@ pub fn setup_tray_icon(app: &mut App) -> Result<(), Box<dyn std::error::Error>> 
 	let _tray = TrayIconBuilder::new()
 		.icon(app.default_window_icon().unwrap().clone())
 		.menu(&menu)
-		.show_menu_on_left_click(true)
+		.show_menu_on_left_click(false)
 		.on_menu_event(|app, event| match event.id.as_ref() {
 			"quit" => {
 				log::info!("quit menu item was clicked");
@@ -19,6 +21,26 @@ pub fn setup_tray_icon(app: &mut App) -> Result<(), Box<dyn std::error::Error>> 
 			}
 			_ => {
 				log::info!("menu item {:?} not handled", event.id);
+			}
+		})
+		.on_tray_icon_event(|tray_icon, event| {
+			if let tauri::tray::TrayIconEvent::Click {
+				button,
+				button_state: MouseButtonState::Down,
+				..
+			} = event
+			{
+				let app_handle = tray_icon.app_handle().clone();
+				if button == MouseButton::Left {
+					match IS_RECORDING.load(Ordering::SeqCst) {
+						false => start_async_task(app_handle),
+						true => stop_async_task(app_handle),
+					}
+				} else if button == MouseButton::Right {
+					if let Err(e) = tray_icon.set_visible(true) {
+						log::warn!("Failed to show tray icon menu: {e}");
+					}
+				}
 			}
 		})
 		.build(app)?;
